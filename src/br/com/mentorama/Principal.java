85d89b3628dd2b36b43ee22c059cdbb4cf09277a
silvaij <isaque.josias@gmail.com>
2020-11-25 21:33:36 -0300

    Finalizado Exercício

--- src/br/com/mentorama/Principal.java
+++ src/br/com/mentorama/Principal.java
@@ -17,50 +17,53 @@ public class Principal {
 			valorDecisao = menu.decisaoDoUsuario();
 
 			switch (valorDecisao) {
-			    case "1": {
-				    System.out.println("Digite o nome da categoria sem espaços");
-				    Scanner scanNomeDaCategoria = new Scanner(System.in);
-				    String nomeDaCategoria = scanNomeDaCategoria.next();
-				    categorias.add(nomeDaCategoria);
-				    break;
-			 }
-			    case "2": {
-				    for (String nome : categorias) {
-				    	
-				    	System.out.println(nome);
-				    }
-					break ;
-				
-			 }
-			    case "3" :{
-			    	
-			    if(!categorias.isEmpty())	
-			    	for(int i =0 ; i<categorias.size(); i++) {
-			    		int cont = i+1;
-			    		System.out.println(cont+" "+categorias.get(i));
-			    	}
-			    	
-			    	Scanner itemSerDeletado = new Scanner(System.in);
-			    	
-			    	System.out.println(" Digite o numero do Item que deseja deletar ");
-			    	int valorDoUsuario = itemSerDeletado.nextInt();
-			    	
-			    	if(valorDoUsuario != 0) {
-			    		int indiceDaLista = valorDoUsuario - 1;
-			    		categorias.remove(indiceDaLista);
-			    		System.out.println("Item Removido da Lista");
-			    	}else {
-			    		System.out.println("Este numero de item não existe na lista");
-			    	}		    	 
-			    	break;
-			    }
-			    case "0": {
-				    System.exit(0);
-			 }
-			    default: {
-				   System.out.println("Opção Invalida");
-			 }
+			case "1": {
+				System.out.println("Digite o nome da categoria sem espaços");
+				Scanner scanNomeDaCategoria = new Scanner(System.in);
+				String nomeDaCategoria = scanNomeDaCategoria.next();
+				categorias.add(nomeDaCategoria);
+				break;
 			}
+			
+			case "2": {
+				for (String nome : categorias) {
+					System.out.println(nome);
+				}
+				break;
+			}
+			
+			case "3": {
+
+				if (!categorias.isEmpty()) {
+					for (int i = 0; i < categorias.size(); i++) {
+						int cont = i + 1;
+						System.out.println(cont + " " + categorias.get(i));
+					}
+
+					Scanner itemSerDeletado = new Scanner(System.in);
+
+					System.out.println(" Digite o numero do Item que deseja deletar ");
+					int valorDoUsuario = itemSerDeletado.nextInt();
+
+					if (valorDoUsuario != 0) {
+						int indiceDaLista = valorDoUsuario - 1;
+						categorias.remove(indiceDaLista);
+						System.out.println("Item Removido da Lista");
+					}else {
+						System.out.println("Este numero de item não existe na lista");
+					}
+				} else {
+                    System.out.println("Lista vazia não existe categoria para remover");
+				}
+				break;
+			}
+			case "0": {
+				System.exit(0);
+			}
+			default: {
+				System.out.println("Opção Invalida"); 
+			 }
+		   }
 		}
 	}
 }
